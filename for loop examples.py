586e4a7beaafddfa3bcae6e746ec1464056d4e31
ElizabetAvila <avilak2173@student.faytechcc.edu>
2024-11-07 13:59:59 -0500

    Add files via upload

---
+++ for loop examples.py
@@ -0,0 +1,38 @@
+
+
+'''
+for var in range (5)
+    print (var+1)
+    #print("apples are delicious!")
+
+'''
+
+'''
+# For loop using the range function with two parameters
+#range function wit two parameters
+end_num = 14
+
+for item in range (7,end_num+1):
+    print(item)
+'''
+'''
+#For loop using the range function with three parameters
+#parameters are start, stop (not incluusive),number to increase by
+for cat in range (0,101,5);
+    print(cat)
+    
+'''
+
+
+myList = ["one", 2, 2.5, "three", 4, 4.5, "five", False]
+
+for thing in myList:
+    print(thing)
+    
+numList = [10, 20, 30, 40, 50]
+
+total = 0
+for num in numList:
+    total = total + num
+
+print(f'the final total is {total}')
