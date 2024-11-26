fd3df16d4c1ddee2c35f3274c97886793baa539d
ElizabetAvila <avilak2173@student.faytechcc.edu>
2024-11-26 14:43:53 -0500

    Add files via upload

---
+++ game example.py
@@ -0,0 +1,19 @@
+
+
+
+def create_character():
+
+    name = input("Enter character name: ")
+    health = int(input(f"Enter {name}'s health: "))
+    invenory = []
+    weapons = ["dagger"]
+    stragnth
+
+    character
+
+def main():
+    print("Game is starting...")
+
+if __name__ == "__main__":
+    main()
+
